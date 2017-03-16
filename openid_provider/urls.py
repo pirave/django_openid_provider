6efdddfa5092eed3883c51aa6f21b51d42087806
Remi <r.fatcheung@gmail.com>
2017-03-16 16:13:31 -0400

    Make package compatible with Django 1.10

--- openid_provider/urls.py
+++ openid_provider/urls.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4 sw=4 : */
 
+import views
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:  # Django < 1.4
     from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('openid_provider.views',
-    url(r'^$', 'openid_server', name='openid-provider-root'),
-    url(r'^decide/$', 'openid_decide', name='openid-provider-decide'),
-    url(r'^xrds/$', 'openid_xrds', name='openid-provider-xrds'),
-    url(r'^(?P<id>.*)/$', 'openid_xrds', {'identity': True}, name='openid-provider-identity'),
-)
+urlpatterns = [
+    url(r'^$', views.openid_server, name='openid-provider-root'),
+    url(r'^decide/$', views.openid_decide, name='openid-provider-decide'),
+    url(r'^xrds/$', views.openid_xrds, name='openid-provider-xrds'),
+    url(r'^(?P<id>.*)/$', views.openid_xrds, {'identity': True}, name='openid-provider-identity'),
+]
