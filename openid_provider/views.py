dd22886709abf30e2fb29bd21da69b780a5b9a7c
Remi <r.fatcheung@gmail.com>
2019-06-04 11:43:12 -0400

    Fix urlparse functions for Python3

--- openid_provider/views.py
+++ openid_provider/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # some code from http://www.djangosnippets.org/snippets/310/ by simon
 # and from examples/djopenid from python-openid-2.2.4
-from future.moves.urllib.parse import urlparse
+from future.moves.urllib.parse import urlparse, urlunparse
 import logging
 from future.moves.urllib.parse import urlencode, quote
 
@@ -209,12 +209,12 @@ def landing_page(request, orequest, login_url=None,
     if not login_url:
         login_url = settings.LOGIN_URL
     path = request.get_full_path()
-    login_url_parts = list(urlparse.urlparse(login_url))
+    login_url_parts = list(urlparse(login_url))
     if redirect_field_name:
         querystring = SafeQueryDict(login_url_parts[4], mutable=True)
         querystring[redirect_field_name] = path
         login_url_parts[4] = querystring.urlencode(safe='/')
-    return HttpResponseRedirect(urlparse.urlunparse(login_url_parts))
+    return HttpResponseRedirect(urlunparse(login_url_parts))
 
 def openid_is_authorized(request, identity_url, trust_root):
     """
