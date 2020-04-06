4fb94424e793401144a77a6ac8dc5987987438a1
Remi <r.fatcheung@gmail.com>
2020-04-06 17:18:17 -0400

    Add on_delete mandatory attribute

--- openid_provider/migrations/0001_initial.py
+++ openid_provider/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('openid', models.CharField(unique=True, max_length=200, blank=True)),
                 ('default', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
                 'ordering': ['openid'],
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('trust_root', models.CharField(max_length=200)),
-                ('openid', models.ForeignKey(to='openid_provider.OpenID')),
+                ('openid', models.ForeignKey(to='openid_provider.OpenID', on_delete=models.CASCADE)),
             ],
         ),
     ]
